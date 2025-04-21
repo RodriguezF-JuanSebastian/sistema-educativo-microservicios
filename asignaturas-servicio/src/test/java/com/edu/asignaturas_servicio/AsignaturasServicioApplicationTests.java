901a562e4fafd617e04fbe494e9b6eaae6cab4fb
Harold <haroldvalencia53@gmail.com>
2025-04-20 23:48:11 -0500

    Modificacion para test de actions

--- asignaturas-servicio/src/test/java/com/edu/asignaturas_servicio/AsignaturasServicioApplicationTests.java
+++ asignaturas-servicio/src/test/java/com/edu/asignaturas_servicio/AsignaturasServicioApplicationTests.java
@@ -2,7 +2,9 @@ package com.edu.asignaturas_servicio;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class AsignaturasServicioApplicationTests {
 
